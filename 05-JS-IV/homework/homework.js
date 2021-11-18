4fae717618d77076b5317dacd91731d7826b0905
abc <abc@gmail.com>
2021-11-18 15:44:26 -0600

    Homework 05 Incomplete

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,16 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+
+  const obj = {
+    nombre: nombre,
+    edad: edad,
+    meow: function(str) {
+      str = 'Meow!';
+      return str;
+    }
+  }
+  return obj;
 }
 
 
@@ -14,6 +24,9 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  
+  objeto[property] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,6 +34,8 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+ objeto[metodo]();
+ 
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
